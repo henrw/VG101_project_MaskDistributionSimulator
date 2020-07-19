371ded0ee554a47b4a6e256c57247c2485ae8ec9
wumuzhe <635717823@qq.com>
2020-07-20 00:21:33 +0800

    Update mask_simulation.h
    add some comment&adjust to plotting

--- mask_simulation.h
+++ mask_simulation.h
@@ -9,50 +9,81 @@
 
 
 // define some const
-static const float k_msk_req = 1.5;
+//static float PI=3.141592653;
+//
+//static float all_scale=1.0f;
 
 using namespace std;
 
 typedef struct CITY
 {
+    string name;
+
     /*for plotting*/
-    float location[2];
-    float radius; //size of the city
+    //the origin point of circle
+    int center_x;
+    int center_y;
+    //the point where the car begins moving
+    int square_x;
+    int square_y;
+    //the point where the city name is printed at
+    int name_x;
+    int name_y;
+    //radius of circle
+    int R;
+    //temporarily ignore this one
+    float k;
+    //the car speed to this city
+    float speed;
+    //the text in dialogue box
+    string text1;
+    string text2;
 
     /* SIR model
-    / Effective Contact Rate: eff_rt= const + k1*(sus_num + inf_num + rec_num)/(2*pi*radius*radius)/cur_msk_num
-    / Recovery Rate: rec_rt= k2*15/hspt_num
-    /   written as functions
+     *
+    / Effective Contact Rate: eff_rt= a + b*(1-e^(-(sus_num + inf_num + rec_num)/(radius*radius*cur_msk_num)))
+    / Recovery Rate: rec_rt= 1/(15+15(1-e^(-inf_num/hspt_num)
+     * these two parameters applies the model 1-e^(-x)
     */
     float sus_num; //those susceptible but not yet infected with the disease
     float inf_num; //infectious (infected) individuals
     float rec_num; //those individuals who have recovered and have immunity
     //Only display the integer part of the number!
 
-    int is_produce;
-    int prod_rt;
-    int cur_msk_num;
-    int req_msk_num;
-
-    int hspt_num; //hospital number
-
-    float beta;
-    float gamma;
+    int is_produce;//whether or not the city produce masks in large scale
+    int prod_rt;//the increment of masks for every period of time
+    int cur_msk_num;//current mask number
+    int req_msk_num;//required mask number
+    int hspt_num;//hospital number
+    float eff_rt;//Effective Contact Rate
+    float rec_rt;//Recovery rate
 
 } _CITY;
 
 typedef struct SYS
 {
-    _CITY *city[2];//city[0] is the mask production city;
+    _CITY *city[2];
     int city_num = 2;
     string time;
 
 } _SYS;
 
+/*function prototypes
+ */
 
+//initialize the system
 void initialization(_SYS *sys1);
+
+// calculate the required mask number for each city
 void mask_require(_SYS *sys1);
+
+// calculate eff_rt, rec_rt in SIR model
 void parameter_renew(_SYS *sys1);
+
+// calculate sus_num, inf_num, rec_num in SIR model
 void sir_renew(_SYS *sys1, float delta_t);
+
+// mask consumed, masks produced, masks transported
 void mask_change(_SYS *sys1, int isTransport);
+
 #endif //CLION_MASK_SIMULATION_H
